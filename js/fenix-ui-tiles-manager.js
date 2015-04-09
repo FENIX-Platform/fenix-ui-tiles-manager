96ed1353c60f93fd22db72b4e4d960bdeb77e312
Kalimaha <guido.barbaglia@gmail.com>
2015-04-09 11:56:15 +0200

    Using JSON Schema to configure the whole application.

--- js/fenix-ui-tiles-manager.js
+++ js/fenix-ui-tiles-manager.js
@@ -2,8 +2,9 @@ define(['jquery',
         'handlebars',
         'text!fenix_ui_tiles_manager/html/templates.html',
         'i18n!fenix_ui_tiles_manager/nls/translate',
+        'text!fenix_ui_tiles_manager/config/tiles_configuration.json',
         'bootstrap',
-        'sweetAlert'], function ($, Handlebars, templates, translate) {
+        'sweetAlert'], function ($, Handlebars, templates, translate, tiles_configuration) {
 
     'use strict';
 
@@ -23,6 +24,15 @@ define(['jquery',
 
         /* Fix the language, if needed. */
         this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'E';
+
+        /* Cast the tiles configuration, if needed. */
+        this.CONFIG.tiles_configuration = tiles_configuration;
+        if (typeof this.CONFIG.tiles_configuration == 'string')
+            this.CONFIG.tiles_configuration = $.parseJSON(this.CONFIG.tiles_configuration);
+
+        console.log(this.CONFIG.tiles_configuration);
+
+        $('#' + this.CONFIG.placeholder_id).html('tiles manager');
 
     };
 
