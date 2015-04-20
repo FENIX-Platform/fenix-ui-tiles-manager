54273aa9e75ea92ea1eaaaae1e5ee3cbbc85b87a
Kalimaha <guido.barbaglia@gmail.com>
2015-04-20 15:30:48 +0200

    Working for the accordion in the tiles manager.

--- nls/translate.js
+++ nls/translate.js
@@ -1,7 +1,13 @@
 define({
     root : {
         error: 'Tiles Manager Error',
-        button_label: 'Bulk Downloads'
+        button_label: 'Bulk Downloads',
+        ghg_title: 'Greenhouse Gases',
+        ghg_description: 'Applications related to GHG',
+        ghg_button: 'Go to the Greenhouse Gases Module',
+        std_title: 'Analysis',
+        std_description: 'Statistical functions and utilities',
+        std_button: 'Go to the Analysis Module'
     },
     F : true,
     S : true
